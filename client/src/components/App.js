9bc6c12fa0b05092c4bf1e4d754d27d3919b7b89
Jesse Jafa <jesse.jafa@gmail.com>
2018-11-13 09:57:17 +0200

    Initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div className="col s12">
+            <div className="row">
+              <div className="input-field col s12">
+                <input type="text" />
+
+              </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
