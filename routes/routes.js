5fb9f6e5439179f3a755d678a9b21f7b3afcb27e
Jesse Jafa <jesse.jafa@gmail.com>
2018-11-26 09:50:16 +0200

    changed api route to better represent api calls

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,18 @@ const convert = require('xml-js');
 const API_KEY = require('../config/keys').GOODREADS_API_KEY;
 
 module.exports = (app) => {
-  app.get('/books/:id', async (req, res) => {
+  app.get('/api/books/:id', async (req, res) => {
     const url = `https://www.goodreads.com/search/index.xml?key=${API_KEY}&q=${req.params.id}`;
     const books = await axios.get(url);
     let json = convert.xml2json(books.data, {compact: true, spaces: 4});
+
+    // let data = [];
+    // for (let i = 0; i < 20; i++) {
+    //   data[i] = {
+    //     title: json["GoodreadsResponse"]["search"]["results"]["work"][i].title._text,
+    //     year: json["GoodreadsResponse"]["search"]["results"]["work"][i].original_publication_year._text,
+    //   }
+    // }
 
     res.send(json);
   })
